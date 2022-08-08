9421b39876787b218419fc10e3c6203b1f474f7c
Sakuracc8 <natalia_m811@hotmail.com>
2022-08-08 04:55:05 -0300

    Routing y Login
    Se configura el routing y se modifica la etiqueta <a href> del botón de Login

--- Portfolio_Natalia_Miguel/FrontEnd/src/app/app.module.ts
+++ Portfolio_Natalia_Miguel/FrontEnd/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { MenuComponent } from './components/menu/menu.component';
 import { BotonIniciarSesionComponent } from './components/boton-iniciar-sesion/boton-iniciar-sesion.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,9 @@ import { HttpClientModule } from '@angular/common/http';
     LogoAPComponent,
     MenuComponent,
     BotonIniciarSesionComponent,
-    BannerComponent
+    BannerComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
