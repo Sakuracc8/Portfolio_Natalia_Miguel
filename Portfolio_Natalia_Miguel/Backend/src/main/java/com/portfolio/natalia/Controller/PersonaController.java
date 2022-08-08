a0d1feac2dfe308ae19d031395fbecdce4645421
Sakuracc8 <natalia_m811@hotmail.com>
2022-08-08 03:22:38 -0300

    Persona model/service
    Se agrega la carpeta model y service con su respectivo contenido.

--- Portfolio_Natalia_Miguel/Backend/src/main/java/com/portfolio/natalia/Controller/PersonaController.java
+++ Portfolio_Natalia_Miguel/Backend/src/main/java/com/portfolio/natalia/Controller/PersonaController.java
@@ -48,4 +48,10 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         return persona;
     }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
+    }
+
 }
