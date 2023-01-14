480a59aca6523db7ee1349895839393cb1bfd666
Sakuracc8 <natalia_m811@hotmail.com>
2023-01-14 00:30:20 -0300

    auth service

--- Portfolio_Natalia_Miguel/FrontEnd/src/app/service/auth.service.ts
+++ Portfolio_Natalia_Miguel/FrontEnd/src/app/service/auth.service.ts
@@ -9,7 +9,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL = 'https://backend-natalia.herokuapp.com/auth/';
+  authURL = 'https://backendportfolio-railway.up.railway.app/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
